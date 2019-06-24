9ac3f42c12310009a71cc9fd719e04edabc03b5b
Isaac Elliott <isaace71295@gmail.com>
2019-06-24 10:05:00 +1000

    notes

--- src/Compile.hs
+++ src/Compile.hs
@@ -3,7 +3,7 @@ module Compile where
 {-
 
 Val *is*    -  data
-Comp *does* -  code
+Comp *does* -  closure
 
 x : T (Ann)            - erased
 x (Var)                - offset into stack
@@ -14,6 +14,14 @@ return[x] (Return)     - produce x as a result
 bind x = a in b (Bind) - run code(a), push its result, run code(b)
 let x = a in b (Let)   - push a, run code(b)
 fix f : T in e (Fix)   - push the start address of code(e), run code(e)
-Case - 
+Case -
+
+const = thunk[ \@(a b : Val) -> \(x : a) -> \(y : b) -> return[x] ]
+
+constZ = thunk[ \@(b : Val) -> force[const] @Nat @b Z[] ]
+
+If I were in the setting where `return[a]` was expecting 2 arguments on the
+stack, then this code wouldn't work properly. I think computations need to be
+closures
 
 -}
